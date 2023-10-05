7d8b1bc618d38c8982a65dd2e49a7e4ec061e7ee
xander <moroz070688@gmail.com>
2023-10-06 00:46:59 +0300

    Decision tree added

--- supervised_ML/classification/IMDB.py
+++ supervised_ML/classification/IMDB.py
@@ -1,60 +1,106 @@
-""" ИЗВЛЕЧЕНИЕ ДАННЫХ """
-
 from keras.datasets import imdb
-
-# Извлечение данных (отзывов) и меток (хороший или плохой отзыв) в учебный и тестовый наборы
-(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
-
-
-""" ПОДГОТОВКА ДАННЫХ """
-
-import numpy as np
-
-def vectorize_sequences(sequences, dimension=10000):
-  """
-  Функция создающая 10000-мерную матрицу из списка закодированных слов
-  """
-  results = np.zeros((len(sequences), dimension))     # Создание матрицы с формой (len(sequences),
-  for i, sequence in enumerate(sequences):
-    results[i, sequence] = 1.                         # Запись единицы в элемент с данным индексом
-  return results
-
-# Подготовка учебных и проверочных данных
-x_train = vectorize_sequences(train_data)             # Векторизованные обучающие данные (данные переведенные в матрицы векторов с 10 000 )
-x_test = vectorize_sequences(test_data)               # Векторизованные контрольные данные
-
-
-# Подготовка учебных и проверочных меток к данным
-y_train = np.asarray(train_labels).astype('float32')  # Векторизованные метки к учебному набору
-y_test = np.asarray(test_labels).astype('float32')    # Векторизованные метки к контрольному набору
-
-# Создание проверочного набора данных
-x_val = x_train[:10000]
-partial_x_train = x_train[10000:]
-
-# Создание проверочного набора меток к данным
-y_val = y_train[:10000]
-partial_y_train = y_train[10000:]
-
-
-""" КОНСТРУИРОВАНИЕ МОДЕЛИ """
 
 from keras import models, layers
 
-model = models.Sequential()
-model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
-model.add(layers.Dense(16, activation='relu'))
-model.add(layers.Dense(1, activation='sigmoid'))
+import numpy as np
 
-# Компиляция модели
-model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
 
-# Обучение модели
-model.fit(x_train,
-          y_train,
-          epochs=4,
-          batch_size=512)
+# Constants
 
-test_loss, test_acc = model.evaluate(x_test, y_test)
+DIMENSION = 10000
 
-print(f'Точность модели на тестовых данных: {test_acc}')
+EPOCHS = 4
+
+BATCH_SIZE = 512
+
+
+def load_imdb_data():
+
+    """Load IMDB data."""
+
+    (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=DIMENSION)
+
+    return train_data, train_labels, test_data, test_labels
+
+
+def vectorize_sequences(sequences, dimension=DIMENSION):
+
+    """
+
+    Create a 10,000-dimensional matrix from a list of encoded words.
+
+    """
+
+    results = np.zeros((len(sequences), dimension))
+
+    for i, sequence in enumerate(sequences):
+
+        results[i, sequence] = 1.  
+
+    return results
+
+
+def prepare_data(train_data, test_data, train_labels, test_labels):
+
+    """Prepare training and testing data."""
+
+    x_train = vectorize_sequences(train_data)  
+
+    x_test = vectorize_sequences(test_data) 
+
+
+    y_train = np.asarray(train_labels).astype('float32')  
+
+    y_test = np.asarray(test_labels).astype('float32')    
+
+
+    x_val = x_train[:DIMENSION]
+
+    partial_x_train = x_train[DIMENSION:]
+
+
+    y_val = y_train[:DIMENSION]
+
+    partial_y_train = y_train[DIMENSION:]
+
+
+    return x_train, x_test, y_train, y_test, x_val, partial_x_train, y_val, partial_y_train
+
+
+def create_model():
+
+    """Construct the model."""
+
+    model = models.Sequential()
+
+    model.add(layers.Dense(16, activation='relu', input_shape=(DIMENSION,)))
+
+    model.add(layers.Dense(16, activation='relu'))
+
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+
+    return model
+
+
+def train_and_evaluate_model(model, x_train, y_train, x_test, y_test):
+
+    """Train and evaluate the model."""
+
+    model.fit(x_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE)
+
+    test_loss, test_acc = model.evaluate(x_test, y_test)
+
+    print(f'Model accuracy on test data: {test_acc}')
+
+
+if __name__ == "__main__":
+
+    train_data, train_labels, test_data, test_labels = load_imdb_data()
+
+    x_train, x_test, y_train, y_test, x_val, partial_x_train, y_val, partial_y_train = prepare_data(train_data, test_data, train_labels, test_labels)
+
+    model = create_model()
+
+    train_and_evaluate_model(model, x_train, y_train, x_test, y_test)
